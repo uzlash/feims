6341b244efaf08239ffeb081eda55a424a0f0027
sadeequekeks <iazubair.ug@atbu.edu.ng>
2020-10-07 12:05:34 +0100

    worked on the sites and equipment

--- src/views/Equipment.vue
+++ src/views/Equipment.vue
@@ -1,11 +1,210 @@
 <template>
-  <div class="equipment">
-    <v-app class="grey lighten-4">
-      <v-container>
-        <h1 class="text-center font-weight-light text-uppercase">Equipments</h1>
-      </v-container>
-    </v-app>
-  </div>
+  <v-app class="custom-background">
+    <v-container>
+      <v-row>
+        <v-col cols="3">
+            <v-card
+              :loading="loading"
+              class="mx-auto my-12"
+              max-width="374"
+            >
+            <template slot="progress">
+              <v-progress-linear
+                color="deep-purple"
+                height="10"
+                indeterminate
+              ></v-progress-linear>
+            </template>
+
+            <v-img
+              height="250"
+              src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+            ></v-img>
+
+            <v-card-title>Cafe Badilico</v-card-title>
+
+              <v-card-text>
+                <v-row
+                  align="center"
+                  class="mx-0"
+                >
+                </v-row>
+
+              <div class="my-4 subtitle-1">
+                $ • Italian, Cafe
+              </div>
+
+              <div>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</div>
+            </v-card-text>
+
+            <v-divider class="mx-4"></v-divider>
+
+            <v-card-title>Tonight's availability</v-card-title>
+
+                <v-card-text>
+                  <v-chip-group
+                    v-model="selection"
+                    active-class="deep-purple accent-4 white--text"
+                    column
+                  >
+                <v-chip>5:30PM</v-chip>
+
+                <v-chip>7:30PM</v-chip>
+
+                <v-chip>8:00PM</v-chip>
+
+                <v-chip>9:00PM</v-chip>
+              </v-chip-group>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-btn
+            color="deep-purple lighten-2"
+            text
+            @click="reserve"
+          >
+            Reserve
+              </v-btn>
+            </v-card-actions>
+          </v-card> 
+        </v-col>
+        <v-col cols="3">
+            <v-card
+              :loading="loading"
+              class="mx-auto my-12"
+              max-width="374"
+            >
+            <template slot="progress">
+              <v-progress-linear
+                color="deep-purple"
+                height="10"
+                indeterminate
+              ></v-progress-linear>
+            </template>
+
+            <v-img
+              height="250"
+              src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+            ></v-img>
+
+            <v-card-title>Cafe Badilico</v-card-title>
+
+              <v-card-text>
+                <v-row
+                  align="center"
+                  class="mx-0"
+                >
+                </v-row>
+
+              <div class="my-4 subtitle-1">
+                $ • Italian, Cafe
+              </div>
+
+              <div>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</div>
+            </v-card-text>
+
+            <v-divider class="mx-4"></v-divider>
+
+            <v-card-title>Tonight's availability</v-card-title>
+
+                <v-card-text>
+                  <v-chip-group
+                    v-model="selection"
+                    active-class="deep-purple accent-4 white--text"
+                    column
+                  >
+                <v-chip>5:30PM</v-chip>
+
+                <v-chip>7:30PM</v-chip>
+
+                <v-chip>8:00PM</v-chip>
+
+                <v-chip>9:00PM</v-chip>
+              </v-chip-group>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-btn
+            color="deep-purple lighten-2"
+            text
+            @click="reserve"
+          >
+            Reserve
+              </v-btn>
+            </v-card-actions>
+          </v-card> 
+        </v-col>
+        <v-col cols="3">
+            <v-card
+              :loading="loading"
+              class="mx-auto my-12"
+              max-width="374"
+            >
+            <template slot="progress">
+              <v-progress-linear
+                color="deep-purple"
+                height="10"
+                indeterminate
+              ></v-progress-linear>
+            </template>
+
+            <v-img
+              height="250"
+              src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+            ></v-img>
+
+            <v-card-title>Cafe Badilico</v-card-title>
+
+              <v-card-text>
+                <v-row
+                  align="center"
+                  class="mx-0"
+                >
+                </v-row>
+
+              <div class="my-4 subtitle-1">
+                $ • Italian, Cafe
+              </div>
+
+              <div>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</div>
+            </v-card-text>
+
+            <v-divider class="mx-4"></v-divider>
+
+            <v-card-title>Tonight's availability</v-card-title>
+
+                <v-card-text>
+                  <v-chip-group
+                    v-model="selection"
+                    active-class="deep-purple accent-4 white--text"
+                    column
+                  >
+                <v-chip>5:30PM</v-chip>
+
+                <v-chip>7:30PM</v-chip>
+
+                <v-chip>8:00PM</v-chip>
+
+                <v-chip>9:00PM</v-chip>
+              </v-chip-group>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-btn
+            color="deep-purple lighten-2"
+            text
+            @click="reserve"
+          >
+            Reserve
+              </v-btn>
+            </v-card-actions>
+          </v-card> 
+        </v-col>
+    </v-row>
+    </v-container>
+    
+  </v-app>
+
 </template>
 
 <script>
@@ -14,3 +213,11 @@ export default {
   components: {}
 }
 </script>
+
+<style scoped>
+.custom-background{
+  background-image: url('../assets/builder.jpg');
+  background-size: cover;
+  background-attachment: fixed;
+}
+</style>
--- src/views/Site.vue
+++ src/views/Site.vue
@@ -1,11 +1,266 @@
 <template>
-  <div class="site">
-    <v-app class="grey lighten-4">
-      <v-container>
-        <h1 class="text-center font-weight-light text-uppercase">Sites</h1>
-      </v-container>
-    </v-app>
-  </div>
+  <v-app class="grey lighten-4">
+    <v-container>
+      <v-row>
+        <v-col cols="3">
+
+          <v-card
+          class="mx-auto"
+          max-width="344"
+        >
+          <v-img
+            src="../assets/Millennium-Tower-abuja.jpg"
+            height="200px"
+          ></v-img>
+
+          <v-card-title>
+            Abuja Millennium Tower
+          </v-card-title>
+
+          <v-card-subtitle>
+            Abuja, Nigeria.
+            <p class="font-weight-bold">Project Cost: 333 Million Dollars.</p>
+          </v-card-subtitle>
+
+          <v-card-actions>
+            <v-btn
+              color="light-blue"
+              text
+            >
+              more
+            </v-btn>
+
+            <v-spacer></v-spacer>
+          </v-card-actions>
+        </v-card>
+        </v-col>
+        <v-col cols="3">
+
+          <v-card
+          class="mx-auto"
+          max-width="344"
+        >
+          <v-img
+            src="../assets/eko-atlantic-city-project.jpg"
+            height="200px"
+          ></v-img>
+
+          <v-card-title>
+            Eko Atlantic
+          </v-card-title>
+
+          <v-card-subtitle>
+            Lagos, Nigeria
+            <p class="font-weight-bold">Project Cost: At least Six (6) Billion Dollars.</p>
+          </v-card-subtitle>
+
+          <v-card-actions>
+            <v-btn
+              color="light-blue"
+              text
+            >
+              more
+            </v-btn>
+
+            <v-spacer></v-spacer>
+          </v-card-actions>
+        </v-card>
+        </v-col>
+        <v-col cols="3">
+
+          <v-card
+          class="mx-auto"
+          max-width="344"
+        >
+          <v-img
+            src="../assets/Onitsha-port.png"
+            height="200px"
+          ></v-img>
+
+          <v-card-title>
+            Dredging of the River Niger
+          </v-card-title>
+
+          <v-card-subtitle>
+            Niger, Delta, Kogi, Anambra.
+            <p class="font-weight-bold">Project Cost: Approximately 280 Million Dollars.</p>
+          </v-card-subtitle>
+
+          <v-card-actions>
+            <v-btn
+              color="light-blue"
+              text
+            >
+              more
+            </v-btn>
+
+            <v-spacer></v-spacer>
+          </v-card-actions>
+        </v-card>
+        </v-col>
+        <v-col cols="3">
+
+          <v-card
+          class="mx-auto"
+          max-width="344"
+        >
+          <v-img
+            src="../assets/Lagos-Light-Rail.jpg"
+            height="200px"
+          ></v-img>
+
+          <v-card-title>
+            Lagos Light Rail
+          </v-card-title>
+
+          <v-card-subtitle>
+            Lagos, Nigeria.
+            <p class="font-weight-bold">Yet to be confirmed(1.20 - 30 Billion Dollars).</p>
+          </v-card-subtitle>
+
+          <v-card-actions>
+            <v-btn
+              color="light-blue"
+              text
+            >
+              more
+            </v-btn>
+
+            <v-spacer></v-spacer>
+          </v-card-actions>
+        </v-card>
+        </v-col>
+        <v-col cols="3">
+
+          <v-card
+          class="mx-auto"
+          max-width="344"
+        >
+          <v-img
+            src="../assets/World-Trade-Centre-Abuja.jpg"
+            height="200px"
+          ></v-img>
+
+          <v-card-title>
+            World Trade Centre
+          </v-card-title>
+
+          <v-card-subtitle>
+            Abuja
+            <p class="font-weight-bold">Project Cost: 1 Billion Dollars.</p>
+          </v-card-subtitle>
+
+          <v-card-actions>
+            <v-btn
+              color="light-blue"
+              text
+            >
+              more
+            </v-btn>
+
+            <v-spacer></v-spacer>
+          </v-card-actions>
+        </v-card>
+        </v-col>
+        <v-col cols="3">
+
+          <v-card
+          class="mx-auto"
+          max-width="344"
+        >
+          <v-img
+            src="../assets/Lekki-Free-Trade-Zone.jpg"
+            height="200px"
+          ></v-img>
+
+          <v-card-title>
+            Lekki Free Trade Zone
+          </v-card-title>
+
+          <v-card-subtitle>
+            Lagos, Nigeria.
+            <p class="font-weight-bold">Project Cost: 700 Million Dollars.</p>
+          </v-card-subtitle>
+
+          <v-card-actions>
+            <v-btn
+              color="light-blue"
+              text
+            >
+              more
+            </v-btn>
+
+            <v-spacer></v-spacer>
+          </v-card-actions>
+        </v-card>
+        </v-col>
+        <v-col cols="3">
+
+          <v-card
+          class="mx-auto"
+          max-width="344"
+        >
+          <v-img
+            src="../assets/power station.jpg"
+            height="200px"
+          ></v-img>
+
+          <v-card-title>
+            Gas Turbine Power Station
+          </v-card-title>
+
+          <v-card-subtitle>
+            Delta, Nigeria.
+            <p class="font-weight-bold">Not Confirmed.</p>
+          </v-card-subtitle>
+
+          <v-card-actions>
+            <v-btn
+              color="light-blue"
+              text
+            >
+              more
+            </v-btn>
+
+            <v-spacer></v-spacer>
+          </v-card-actions>
+        </v-card>
+        </v-col>
+        <v-col cols="3">
+
+          <v-card
+          class="mx-auto"
+          max-width="344"
+        >
+          <v-img
+            src="../assets/Abuja-Gateway-Airport1.jpg"
+            height="200px"
+          ></v-img>
+
+          <v-card-title>
+            Abuja Gateway Airport
+          </v-card-title>
+
+          <v-card-subtitle>
+            Abuja, Nigeria.
+            <p class="font-weight-bold">Project Cost: 371 Million Dollars.</p>
+          </v-card-subtitle>
+
+          <v-card-actions>
+            <v-btn
+              color="light-blue"
+              text
+            >
+              more
+            </v-btn>
+
+            <v-spacer></v-spacer>
+          </v-card-actions>
+        </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
 </template>
 
 <script>

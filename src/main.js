935c414301ac909aef3a98f64c8d90299ebe5ad8
Usman Murtala <uzlash16@gmail.com>
2020-10-07 12:03:50 +0100

    worked on facility route

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import VueGoogleCharts from 'vue-google-charts'
+ 
+Vue.use(VueGoogleCharts)
 
 Vue.config.productionTip = false
 

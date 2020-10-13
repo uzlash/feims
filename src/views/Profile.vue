a8d6fc63e24e4acb9dcd37efd2100d34c203f66e
umarrg <umarabubakarfta@gmail.com>
2020-10-13 14:40:50 +0100

    update in profile

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -2,45 +2,57 @@
   <v-app class="custom__style">
     <v-container>
       <v-row>
-        <v-col cols="10">
-          <div>
-            <v-col cols="12">
-              <v-avatar size="150" color="">
+        <v-col cols="9" md8>
+          <v-col cols="9">
+            <v-card class="overflow-hidden">
+              <v-toolbar flat color="light-blue">
+                <v-icon left color="white">mdi-account</v-icon>
+                <v-toolbar-title class="white--text">
+                  User Profile
+                </v-toolbar-title>
+                <v-spacer></v-spacer>
+              </v-toolbar>
+              <v-card-text>
+                <span>Name</span>
+                <v-text-field
+                  color="light-blue"
+                  label="Name"
+                  solo
+                  prepend-inner-icon="mdi-pen"
+                ></v-text-field>
+              </v-card-text>
+              <v-card-text>
+                <span>Email</span>
+                <v-text-field
+                  color="light-blue"
+                  label="Email"
+                  solo
+                  prepend-inner-icon="mdi-email"
+                ></v-text-field>
+              </v-card-text>
+              <v-card-text>
+                <span>Address</span>
+                <v-text-field
+                  color="light-blue"
+                  label="Address"
+                  solo
+                  prepend-inner-icon="mdi-home"
+                ></v-text-field>
+              </v-card-text>
+              <v-divider></v-divider>
+            </v-card>
+          </v-col>
+        </v-col>
+        <v-col cols="3">
+          <v-card height="300">
+            <v-card class="text-center mt-5" flat>
+              <v-avatar size="100" color="">
                 <img src="../assets/avatar-1.jpg" alt="alt" />
               </v-avatar>
-            </v-col>
-          </div>
-          
-          <v-col cols="10">
-            <div class="text-center">
-              <v-col cols="10">
-                <v-text-field solo label="Name" prepend-inner-icon="mdi-pen">
-                </v-text-field>
-              </v-col>
-              <v-col cols="10">
-                <v-text-field solo label="Email" prepend-inner-icon="mdi-email">
-                </v-text-field>
-              </v-col>
-              <v-col cols="10">
-                <v-text-field solo label="phone" prepend-inner-icon="mdi-phone">
-                </v-text-field>
-              </v-col>
-              <v-col cols="10">
-                <v-text-field
-                  solo
-                  label="Adderss"
-                  prepend-inner-icon="mdi-home"
-                >
-                </v-text-field>
-
-                <v-row justify="end">
-                  <v-btn color="light-blue" class="white--text"
-                    >Save changes</v-btn
-                  >
-                </v-row>
-              </v-col>
-            </div>
-          </v-col>
+              <h3 class="">keiko</h3>
+              <h6>keiko ng</h6>
+            </v-card>
+          </v-card>
         </v-col>
       </v-row>
     </v-container>
